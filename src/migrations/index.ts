18e678ec3b7d38ab59303984770a15dcc86863ee
Jeremy Sarda <jcsarda+hackur@gmail.com>
2025-03-28 05:07:57 -0700

    fix: add admin migration to index and PAYLOAD_SECRET to vercel.json

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,4 +1,5 @@
 import * as migration_20250328_112540 from './20250328_112540';
+import * as migration_20250328_admin from './20250328_admin';
 import * as migration_seed from './seed';
 
 export const migrations = [
@@ -6,6 +7,11 @@ export const migrations = [
     up: migration_20250328_112540.up,
     down: migration_20250328_112540.down,
     name: '20250328_112540',
+  },
+  {
+    up: migration_20250328_admin.up,
+    down: migration_20250328_admin.down,
+    name: '20250328_admin',
   },
   {
     up: migration_seed.up,
